7377156e0a725b97ab0049acc89bf16d53413adf
LasseThostrup <201271288@iha.dk>
2014-04-14 13:24:08 +0200

    Master og Slave kode
    Ændring af mappe struktur

---
+++ Software/X.10_Slave/X.10_Slave/X.10_Slave.c
@@ -0,0 +1,17 @@
+﻿/*
+ * X.c
+ *
+ * Created: 14-04-2014 13:19:56
+ *  Author: Lasse
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
