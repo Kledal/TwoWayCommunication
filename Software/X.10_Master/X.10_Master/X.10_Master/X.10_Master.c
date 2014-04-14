abbe001ea8b067777c87df8755436e0bf3b12584
LasseThostrup <201271288@iha.dk>
2014-04-14 13:18:52 +0200

    Kode, Master og Slave
    Oprettelse af klasser mm.

---
+++ Software/X.10_Master/X.10_Master/X.10_Master/X.10_Master.c
@@ -0,0 +1,17 @@
+﻿/*
+ * X.c
+ *
+ * Created: 14-04-2014 13:16:20
+ *  Author: Lasse
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
