df20b040669c60c1eb40842fe6aea5ccf1a7c354
LarsHarupHolm <201370744@iha.dk>
2014-04-14 14:02:49 +0200

    Slave v 0.1
    Tomme funktioner og Door

---
+++ Software/X.10_Slave/X.10_Slave/Door/Door.c
@@ -0,0 +1,22 @@
+/* Created: 14-04-2014
+ * Name: Door.c
+ * Version 0.1
+ */
+
+#include "Door.h"
+
+void changeStatus( int status )	//from slave to door
+{
+	
+}
+
+void toggleDoor( void )
+{
+	
+}
+
+int getStatus( void )
+{
+	
+	return status;
+}
